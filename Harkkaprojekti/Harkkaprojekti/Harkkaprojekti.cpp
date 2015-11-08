634e53512e92f59bc360602337ef01430b9838c1
Jardei <janneuusipohjola@gmail.com>
2015-11-08 22:24:55 +0200

    ohjelma tervehtii

--- Harkkaprojekti/Harkkaprojekti/Harkkaprojekti.cpp
+++ Harkkaprojekti/Harkkaprojekti/Harkkaprojekti.cpp
@@ -2,10 +2,13 @@
 //
 
 #include "stdafx.h"
-
+#include <stdlib.h>
+#include "Hello.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Hello hei = Hello();
+	system("PAUSE");
 	return 0;
 }
 
---
+++ Harkkaprojekti/Harkkaprojekti/Hello.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "Hello.h"
+#include <iostream>
+
+using namespace std;
+
+Hello::Hello()
+{
+	cout << "Terve! Tan on tehnyt Janne Uusi-Pohjola ja Kalle Vehmaanpera\n";
+}
+
+
+Hello::~Hello()
+{
+}
